ac7afbe47916521dd46c400b7ecff2e13e8ea56a
Usama Ansari <ua92989@gmail.com>
2026-06-11 01:02:40 +0500

    fixed attendance routes

--- backend/src/modules/attendance/attendance.routes.js
+++ backend/src/modules/attendance/attendance.routes.js
@@ -3,6 +3,6 @@ import { createAttendance } from './attendance.controller.js';
 
 const router = express.Router();
 
-router.post('/', createAttendance);
+router.post('/save', createAttendance);
 
 export default router;
