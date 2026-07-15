f770aae20c4208f71b178574d1fff28d274a4f71
Usama Ansari <ua92989@gmail.com>
2026-07-16 03:30:31 +0500

    change base url in frontend attendance.service.ts

--- frontend/services/attendance.service.ts
+++ frontend/services/attendance.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "https://portal.stevta.gos.pk/api/v1",
     withCredentials: true,
 });
 
